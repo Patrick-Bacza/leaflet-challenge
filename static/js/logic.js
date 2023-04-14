1fa2f58f788f85d0a7e0f803d2f1168d77fca1db
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-04-13 23:18:42 -0400

    work

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,12 +13,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap)
 
+d3.json(url).then(function(data) {
 
-
+// Create the marker size using the magnitude
   function markerSize(earthquakes) {
-    return earthquakes * 10000
+    return earthquakes * 5
   }
 
+  
+
+  // Create makrer color based on depth
   function markerColor(earthquakes) { 
     if (earthquakes >= -10 && earthquakes < 10){
       return "green"
@@ -29,43 +33,73 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     else if(earthquakes >= 30 && earthquakes < 70){
       return "orange"
     }
-    else if (earthquakes >= 70 && earthquakes < 90){
-      return "red"
-    }
-    else if (earthquakes >= 90){
+
+    else if (earthquakes >= 70){
       return "red"
     }
 }
 
+function markerOpacity(earthquakes){
+        if (earthquakes >= -10 && earthquakes < 10){
+          return .40
+        }
+        else if (earthquakes >= 10 && earthquakes < 30){
+          return .60
+        }
+        else if(earthquakes >= 30 && earthquakes < 70){
+          return .70
+        }
+    
+        else if (earthquakes >= 70){
+          return .9
+        }
 
-
-let earthquakeMarkers = []
-
-  d3.json(url).then(function(data) {
+}
 
     L.geoJson(data , {
       pointToLayer: function(feature, latlng){
-        console.log(data);
+        console.log(latlng);
         return L.circleMarker(latlng)
       },
 
       style: function(feature) {
         return {
-            color: false,
-            fillColor: "green",
-            //radius: markerSize(feature.properties.mag)
+          opacity: 1,
+          fillOpacity: markerOpacity(feature.geometry.coordinates[2]),
+          fillColor: markerColor(feature.geometry.coordinates[2]),
+          color: "green",
+          radius: markerSize(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
 
-        }
       },
+      
       onEachFeature: function(feature , layer) {
       layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}`)
       }
 
     }).addTo(myMap)
       
-       
-        
-      });
+
+
+// legend 
+
+let legend = L.control({
+  position: "bottomright"
+});
+
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div" , "info legend")
+  let depth = ['-10-10' , '10-30' , '30-70' , '70+'] 
+  let  colors = ['green' , 'yellow' , 'orange' , 'red']
+  let labels = []
+  
+}
+
+ 
+  legend.addTo(map);
+});
 
 
 
